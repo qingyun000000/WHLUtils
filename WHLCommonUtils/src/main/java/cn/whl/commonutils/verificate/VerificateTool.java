954916a247f051f10588432f33d65f226e652622
qingyun000000 <32904713+qingyun000000@users.noreply.github.com>
2019-12-06 10:47:39 +0800

    累计更新 2019-12-6 2
    实现格式校验；

--- WHLCommonUtils/src/main/java/cn/whl/commonutils/verificate/VerificateTool.java
+++ WHLCommonUtils/src/main/java/cn/whl/commonutils/verificate/VerificateTool.java
@@ -12,7 +12,22 @@ public class VerificateTool {
      * @return
      */
     public static boolean isCellPhone(String str){
-        
+        if(str == null || str.trim().length() == 0){
+            return false;
+        }
+        String phone;
+        if(str.startsWith("+86")){
+            phone = str.substring(3);
+        }else if(str.startsWith("86")){
+            phone = str.substring(2);
+        }else{
+            phone = str;
+        }
+        if(!phone.startsWith("13") && !phone.startsWith("15") && !phone.startsWith("17") && !phone.startsWith("18")){
+            return false;
+        }else if(phone.matches("[1-9]{2}[0-9]{9}")){
+            return true;
+        }
         return false;
     }
     
@@ -43,7 +58,27 @@ public class VerificateTool {
      * @return
      */
     public static boolean isNumber(String str){
-       
+        if(str == null || str.trim().length() == 0){
+            return false;
+        }
+        if(str.matches("[0-9]+(.[0-9]+)")){
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * 整数校验
+     * @param str
+     * @return
+     */
+    public static boolean isInt(String str){
+        if(str == null || str.trim().length() == 0){
+            return false;
+        }
+        if(str.matches("[0-9]+")){
+            return true;
+        }
         return false;
     }
     
@@ -53,7 +88,12 @@ public class VerificateTool {
      * @return
      */
     public static boolean isEmail(String str){
-       
+        if(str == null || str.trim().length() == 0){
+            return false;
+        }
+        if(str.matches("[A-Za-z0-9_]+@[A-Za-z0-9]+\\.[A-Za-z]+")){
+            return true;
+        }
         return false;
     }
     
@@ -63,7 +103,12 @@ public class VerificateTool {
      * @return
      */
     public static boolean isIPv4(String str){
-        
+        if(str == null || str.trim().length() == 0){
+            return false;
+        }
+        if(str.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")){
+            return true;
+        }
         return false;
     }
     
@@ -73,6 +118,9 @@ public class VerificateTool {
      * @return
      */
     public static boolean isIPv6(String str){
+        if(str == null || str.trim().length() == 0){
+            return false;
+        }
        
         return false;
     }
@@ -85,6 +133,9 @@ public class VerificateTool {
      * @return
      */
     public static boolean coinLength(String str, int minLength, int maxLength){
+        if(str == null || str.trim().length() == 0 ){
+            return false;
+        }
         if(str.length() >= minLength && str.length() <= maxLength){
             return true;
         }
