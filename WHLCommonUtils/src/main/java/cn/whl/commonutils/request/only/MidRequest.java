bc16f1b5ba84eabe08e346cedc7d2cb938b2d5e6
qingyun000000 <32904713+qingyun000000@users.noreply.github.com>
2020-03-06 16:17:25 +0800

    累计更新 2020-3-6 -2

---
+++ WHLCommonUtils/src/main/java/cn/whl/commonutils/request/only/MidRequest.java
@@ -0,0 +1,20 @@
+package cn.whl.commonutils.request.only;
+
+/**
+ * mid封装
+ * @author wuhailong
+ */
+public class MidRequest {
+    
+    private Long mid;
+
+    public Long getMid() {
+        return mid;
+    }
+
+    public void setMid(Long mid) {
+        this.mid = mid;
+    }
+    
+    
+}
