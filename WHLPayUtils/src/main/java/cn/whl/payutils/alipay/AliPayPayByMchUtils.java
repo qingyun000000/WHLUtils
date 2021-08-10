8c81cffb39d39ddc1400363c3af33c488bc25d36
qingyun000000 <32904713+qingyun000000@users.noreply.github.com>
2021-08-10 14:46:42 +0800

    2021-8-10 -1

--- WHLPayUtils/src/main/java/cn/whl/payutils/alipay/AliPayPayByMchUtils.java
+++ WHLPayUtils/src/main/java/cn/whl/payutils/alipay/AliPayPayByMchUtils.java
@@ -2,14 +2,18 @@ package cn.whl.payutils.alipay;
 
 import cn.whl.payutils.alipay.vo.accountQuery.AliPayAccountQueryIn;
 import cn.whl.payutils.alipay.vo.accountQuery.AliPayAccountQueryOut;
+import cn.whl.payutils.alipay.vo.pay.AliPayPayOut;
 import cn.whl.payutils.alipay.vo.payByMch.AliPayPayByMchIn;
 import cn.whl.payutils.alipay.vo.payByMch.AliPayPayByMchOut;
 import cn.whl.payutils.alipay.vo.payByMch.AliPayPayByMchQueryIn;
 import cn.whl.payutils.alipay.vo.payByMch.AliPayPayByMchQueryOut;
+import cn.whl.payutils.enums.PayType;
+import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
 import com.alipay.api.domain.AlipayFundTransUniTransferModel;
 import com.alipay.api.request.AlipayFundTransUniTransferRequest;
+import com.alipay.api.response.AlipayFundTransUniTransferResponse;
 
 /**
  * 阿里支付订单退款方法
@@ -17,7 +21,7 @@ import com.alipay.api.request.AlipayFundTransUniTransferRequest;
  */
 public class AliPayPayByMchUtils {
 
-    static AliPayPayByMchOut payByMch(AliPayPayByMchIn in) {
+    static AliPayPayByMchOut payByMch(AliPayPayByMchIn in) throws AlipayApiException, Exception {
         AlipayClient alipayClient = new DefaultAlipayClient(in.getServerUrl(),in.getAppId(), in.getPrivateKey(), in.getFormat(), in.getCharSet(), in.getAlipayPublicKey(), in.getSignType());
         AlipayFundTransUniTransferRequest request = new AlipayFundTransUniTransferRequest();
         
@@ -31,9 +35,31 @@ public class AliPayPayByMchUtils {
         if(in.getPayeeInfo()!= null ){
             model.setPayeeInfo(in.getPayeeInfo());
         }
+        request.setBizModel(model);
         
-        AliPayPayByMchOut out = new AliPayPayByMchOut();
-        return out;
+        AlipayFundTransUniTransferResponse response = alipayClient.execute(request);
+        
+        System.out.println(response.isSuccess());
+        System.out.println(response.getCode());
+        System.out.println(response.getMsg());
+        System.out.println(response.getSubMsg());
+        System.out.println(response.getOutBizNo());
+        System.out.println(response.getStatus());
+        System.out.println(response.getBody());
+        
+        if (response.isSuccess()) {
+            System.out.println("调用成功");
+            AliPayPayByMchOut out = new AliPayPayByMchOut();
+            out.setPayType(PayType.PAY);
+            out.setCode(response.getCode());
+            out.setMsg(response.getMsg());
+            out.setSubCode(response.getSubMsg());
+            out.setSubMsg(response.getSubMsg());
+            return out;
+        } else {
+            System.out.println(response);
+            throw new Exception("支付失败");
+        }
     }
 
     static AliPayPayByMchQueryOut query(AliPayPayByMchQueryIn payByMchQueryParams) {
